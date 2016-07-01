57e5b7680c08adbac8335ddd39244ea894cdbb18
Midi Kang <midi13@163.com>
2016-07-01 13:45:36 +0800

    add validation for baggage allowance for seat model

--- app/models/seat.rb
+++ app/models/seat.rb
@@ -1,2 +1,10 @@
 class Seat < ActiveRecord::Base
+  validate :check_baggage_allowance
+  
+  private
+    def check_baggage_allowance
+      if baggage > Flight.find(flight_id).baggage_allowance
+        errors.add(:baggage, "You have too much baggage")
+      end
+    end
 end

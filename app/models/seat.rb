c8b4831ca47e95ace0b4f055ec6deb5e488e5526
Midi Kang <midi13@163.com>
2016-07-01 14:25:52 +0800

    add flight capacity validation while booking

--- app/models/seat.rb
+++ app/models/seat.rb
@@ -1,11 +1,15 @@
 class Seat < ActiveRecord::Base
   belongs_to :flight
-  validate :check_baggage_allowance
+  validate :check_booking
   
   private
-    def check_baggage_allowance
+    def check_booking
       if baggage > flight.baggage_allowance
         errors.add(:baggage, "You have too much baggage")
+      end
+      
+      if flight.seats.size >= flight.capacity
+        errors.add(:name,"The flight is fully booked")
       end
     end
 end

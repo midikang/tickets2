4294050173d8b092b55e448ad32ba698a9c27aaf
Midi Kang <midi13@163.com>
2016-07-14 23:02:43 +0800

    javascript Server-Side Concerns

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -33,6 +33,7 @@ class FlightsController < ApplicationController
     respond_to do |format|
       if @flight.save
         format.html { redirect_to @flight, notice: 'Flight was successfully created.' }
+        format.js
         format.json { render :show, status: :created, location: @flight }
       else
         format.html { render :new }

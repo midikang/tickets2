6c15ff0ff6f52c8d30c367f0359c5752d97a218a
Midi Kang <midi13@163.com>
2016-07-15 14:46:54 +0800

    link index

---
+++ app/controllers/links_controller.rb
@@ -0,0 +1,14 @@
+class LinksController < ApplicationController
+  def index
+    @links = Link.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
+  resources :links
+
   root 'welcome#index'
 
   #get 'categorires/index'

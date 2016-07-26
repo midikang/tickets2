38ca40dff34abb79ae2241e033c8d7c42fb69d4f
Midi Kang <midi13@163.com>
2016-07-26 11:00:11 +0800

    call back demo

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,3 +1,19 @@
 class Menu < ActiveRecord::Base
   has_many  :menuItems
+  
+  around_create :my_create_check
+  
+  after_initialize do |menu|
+    puts "You have initialized an object!"
+  end
+ 
+  after_find do |menu|
+    puts "You have found an object!"
+  end
+  
+  protected
+    def my_create_check
+      puts "around_create an Menu object"
+    end
+  
 end

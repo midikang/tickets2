83b8b0699a362a0d081def06805cfaf53d203d90
Midi Kang <midi13@163.com>
2016-08-09 15:43:05 +0800

    scafolld post
    1. extract post show to partil
    2. link_to_unless_current
    3. simple_format content

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160726031313) do
+ActiveRecord::Schema.define(version: 20160809072209) do
 
   create_table "addresses", force: :cascade do |t|
     t.string   "kind"
@@ -121,6 +121,13 @@ ActiveRecord::Schema.define(version: 20160726031313) do
 
   create_table "people", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

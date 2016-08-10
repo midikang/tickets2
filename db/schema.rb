a49117cb14a0a790d8f719f009e1095dd35d7976
Midi Kang <midi13@163.com>
2016-08-10 16:23:40 +0800

    add post

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160809072209) do
+ActiveRecord::Schema.define(version: 20160809074900) do
 
   create_table "addresses", force: :cascade do |t|
     t.string   "kind"
@@ -75,6 +75,15 @@ ActiveRecord::Schema.define(version: 20160809072209) do
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
   end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "post_id"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
 
   create_table "flights", force: :cascade do |t|
     t.datetime "departure"
